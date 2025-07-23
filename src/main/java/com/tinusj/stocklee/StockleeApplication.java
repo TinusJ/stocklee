fbec622b52218d9a6ab7d5be0ecd851a9057d1ac
Tinus <jacksontinus96@gmail.com>
2025-07-23 11:59:20 +0200

    init

---
+++ src/main/java/com/tinusj/stocklee/StockleeApplication.java
@@ -0,0 +1,13 @@
+package com.tinusj.stocklee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockleeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockleeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tinusj/stocklee/StockleeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinusj.stocklee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockleeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
